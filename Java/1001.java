8c8b75238d8ca85b185d3348b03aeefd2b5a6131
Yuri Akira Shiba <61916114+yuri-akira@users.noreply.github.com>
2020-03-27 17:40:58 -0300

    Create 1001.java

---
+++ Java/1001.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+       int A,B;
+       Scanner input = new Scanner(System.in);
+       B = input.nextInt();
+       A = input.nextInt();
+       
+       System.out.println("X = "+(A+B));
+    }
+ 
+}
